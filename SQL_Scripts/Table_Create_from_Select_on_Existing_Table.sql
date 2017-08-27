713b044a34feae95dc42e815e77728768932906b
Joshua K Jones <joshuakevinjones@gmail.com>
2017-08-27 17:37:32 -0400

    Update Table_Create_from_Select_on_Existing_Table.sql

--- SQL_Scripts/Table_Create_from_Select_on_Existing_Table.sql
+++ SQL_Scripts/Table_Create_from_Select_on_Existing_Table.sql
@@ -1,10 +1,10 @@
------Creating table SEC_INTERNAL_AUDIT
+--Creating table SEC_INTERNAL_AUDIT
 
 --creating table with selected results from another table
 --could use to filter, etc.
 
 SELECT DISTINCT 
-	[Empl ID]
+	 [Empl ID]
 	,[Dept Descr]
 
 INTO 
@@ -14,4 +14,4 @@ FROM
 [IAACE1].[TRNPAY].[BASE_JOB_INFO_DETAIL]
 
 WHERE 
-[Dept Descr] = 'Internal Audit';
+[Dept Descr] = 'Internal Audit'
