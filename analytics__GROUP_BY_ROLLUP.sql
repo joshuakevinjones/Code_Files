fc607297e6efe682114a84d1b6c098c666be7a62
Joshua K Jones <joshuakevinjones@gmail.com>
2017-08-27 17:25:37 -0400

    Update analytics__GROUP_BY_ROLLUP.sql

--- analytics__GROUP_BY_ROLLUP.sql
+++ analytics__GROUP_BY_ROLLUP.sql
@@ -1,15 +1,15 @@
 SELECT  
 	 DATEPART(YYYY, [Check Dt]) AS 'Year'
-	, DATEPART(MM, [Check Dt]) AS 'Period'    
-	, [Chk Option Descr] 
-	, [Off Cycle]
+	,DATEPART(MM, [Check Dt]) AS 'Period'    
+	,[Chk Option Descr] 
+	,[Off Cycle]
 	--,[Reprint]
 	--,[Pay Period End]
-	, SUM([Tot Gross]) AS 'Tot Gross'
-	, SUM([Total Taxes]) AS 'Total Taxes'
-	, SUM([Total Ded#]) AS 'Total Ded'
-	, SUM([Net Pay]) AS 'Net Pay'
-	, Count([Check Nbr]) AS 'Number of Checks'
+	,SUM([Tot Gross]) AS 'Tot Gross'
+	,SUM([Total Taxes]) AS 'Total Taxes'
+	,SUM([Total Ded#]) AS 'Total Ded'
+	,SUM([Net Pay]) AS 'Net Pay'
+	,Count([Check Nbr]) AS 'Number of Checks'
 	--,[Check Nbr]
 
 FROM
@@ -17,11 +17,11 @@ FROM
 
 GROUP BY 
 	ROLLUP
-	(
-	  DATEPART(YYYY, [Check Dt])
-	, DATEPART(MM, [Check Dt])   
-	, [Chk Option Descr] 
-	, [Off Cycle]
-	--,[Reprint]
-	--,[Pay Period End]
-	)
+		(
+			 DATEPART(YYYY, [Check Dt])
+			,DATEPART(MM, [Check Dt])   
+			,[Chk Option Descr] 
+			,[Off Cycle]
+			--,[Reprint]
+			--,[Pay Period End]
+		)
