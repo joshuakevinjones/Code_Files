6e088bd699ec4da4ad7eeaf332cbc03ccf4a749a
Joshua K Jones <joshuakevinjones@gmail.com>
2017-08-27 17:28:16 -0400

    Update and rename Table_CreateNew_CopyExistingInto.txt to Table_CreateNew_CopyExistingInto.sql

--- SQL_Scripts/Table_CreateNew_CopyExistingInto.sql
+++ SQL_Scripts/Table_CreateNew_CopyExistingInto.sql
@@ -1,9 +1,12 @@
 -- Direct table copy with rename and copy of all data
 -- Also creates the new table
 
-SELECT * 
-	INTO [IAACE1].[dbo].[JKJ_OFAC_AKA_BASE_RESULTS] 
-FROM [IAACE1].[SAP].[OFAC_AKA_BASE_RESULTS];
+SELECT 
+* 
+	INTO 
+	[IAACE1].[dbo].[JKJ_OFAC_AKA_BASE_RESULTS] 
+	FROM 
+	[IAACE1].[SAP].[OFAC_AKA_BASE_RESULTS];
 
 --TRUNCATE TABLE [IAACE1].[dbo].[JKJ_OFAC_AKA_BASE_RESULTS];
 
